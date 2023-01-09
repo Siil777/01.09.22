ffbbb211b9d9f71de0e46e4ccaf9493cb3baae57
opilane <opilane@HP-CZC2349HTD>
2023-01-09 17:15:08 +0200

    Add project files.

---
+++ _01.09.22.py
@@ -0,0 +1,55 @@
+﻿#from random import randint
+#import random
+from module1 import *
+
+#t=['+','-','/']
+#while True:
+#    y=random.choice(t)
+#    i=randint(1,10)
+#    try:
+
+
+
+
+#answer=arithmetic(2.5, '+', 1.6)
+#print(answer)
+
+#while True:
+#    answer=arithmetic(input(),input(), input())
+#    print(answer)
+
+
+
+p,s,d =square(input())
+
+print(f'Rectangle:{p}')
+print(f'area:{s}')
+print(f'diagonal:{d}')
+
+
+##4Времена года
+##Написать функцию season, принимающую 1 аргумент — номер месяца (от 1 до 12), и возвращающую время года,
+##которому этот месяц принадлежит (talv, kevad, suvi или sügis).
+
+
+
+#month=input('month(day):')
+
+while True:
+    if not month.isdigit():
+        print('incorrect input')
+        print(' you can input only integer')
+        month= input('month(day):')
+        continue 
+    else:
+        break
+
+
+
+
+
+
+
+
+
+
